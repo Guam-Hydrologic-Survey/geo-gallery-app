30923c9f5c1c5eabebb2654862fb51ab27b3fe45
Dannika <71364766+dannikate@users.noreply.github.com>
2026-03-13 14:02:34 +1000

    code cleanup

--- client/src/main.js
+++ client/src/main.js
@@ -151,24 +151,11 @@ document.getElementById('locate').addEventListener('click', () => {
     alert('Clicked on locate');
 });
 
-// getSurveyPoints();
-// getSurveyPolygons();
-
 getLayers("./src/data/points.json")
 getLayers("./src/data/polygons.json")
 
-// getPolyPoints();
-
 /* ------------------------------------------------------------ */
 /* ------------------------------------------------------------ */
-
-// function getPolyPoints() {
-//     fetch(`./src/data/polygons-points.json`)
-//     .then(response => response.json())
-//     .then(data => {
-//         L.geoJSON(data).addTo(map);
-//     })
-// }
 
 function getLayers(data) {
     fetch(data) 
@@ -196,63 +183,6 @@ function getLayers(data) {
         }).addTo(map);
     });
 }
-
-// function getSurveyPoints() {
-//     fetch(`./src/data/points.json`) 
-//     .then(response => response.json())
-//     .then(data => {
-//         L.geoJSON(data, {
-//             onEachFeature: (feature, layer) => {
-//                 layer.on('click', async () => {
-//                     console.log(`Clicked on ${feature.properties.description} with ID ${feature.properties.id}`);
-//                     findImagesSet_v2('/api/photos/', feature.properties.name).then(images => {
-
-//                         document.getElementById("point-clicked").innerText = `${feature.properties.description}`;
-
-//                         if (images.paths != null) {
-//                             displayImages_v3(images.paths);
-//                         } else {
-//                             console.log(`Sorry, could not find images :-(`);
-//                         }
-
-//                         modalDialog.show();
-//                     });
-//                     // getImageDescription_v2(feature.properties.id);
-//                 });
-//             }
-//         }).addTo(map);
-//     });
-// }
-
-// function getSurveyPolygons() {
-//     fetch(`./src/data/polygons.json`) 
-//     .then(response => response.json())
-//     .then(data => {
-//         L.geoJSON(data, {
-//             onEachFeature: (feature, layer) => {
-//                 layer.on('click', async () => {
-//                     console.log(`Clicked on ${feature.properties.description} with ID ${feature.properties.id}`);
-//                     console.log(feature.properties.images);
-//                     console.log(feature.properties.id, feature.properties.name)
-//                     findImagesSet('/api/photos/', feature.properties.name).then(images => {
-//                         console.log(images)
-//                         document.getElementById("point-clicked").innerText = `${feature.properties.description}`;
-
-//                         if (images.paths != null) {
-//                             console.log("Found pictures!")
-//                             displayImages_v3(images.paths);
-//                         } else {
-//                             console.log(`Sorry, could not find images :-(`);
-//                         }
-
-//                         modalDialog.show();
-//                     });
-//                     getImageDescription_v2(feature.properties.id);
-//                 });
-//             }
-//         }).addTo(map);
-//     });
-// }
 
 // for method 4
 async function findImagesSet(apiUrl, searchId) {
