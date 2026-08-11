0c91f3215eab4f2ce6a3cb69beb05cf2f720758c
Dannika <71364766+dannikate@users.noreply.github.com>
2026-08-11 15:43:35 +1000

    continue conversion to js creation of modal elements
    - change tr_btn properties to setAttribute for clarity
    - build tr_btn_grp by appending tr_btn, toggle_dropdown, and dropdown_menu
    - create elements for modal: header, body, and footer
    - note: content is wrapper for header, body, and footer; content_wrapper is as name suggests (this is needed for bootstrap, otherwise, errors are thrown)
    - comment out orig modal inner HTML (the raw HTML string)

--- client/src/components/About2.js
+++ client/src/components/About2.js
@@ -35,18 +35,12 @@ export function About() {
       }
     ];
 
-    const description = "";
-
     const tr_btn_grp = document.createElement("div");
     tr_btn_grp.className = "btn-group";
 
     // btn for technical report 
     const tr_btn = document.createElement("a");
     tr_btn.className = "btn btn-primary";
-    tr_btn.title = "Coming soon!"; // update to tr.title when available 
-    tr_btn.href = tr.url;
-    tr_btn.target 
-
     tr_btn.setAttribute("title", "Coming soon!"); // update to tr.title when available 
     tr_btn.setAttribute("href", tr.url);
     tr_btn.setAttribute("target", "_blank");
@@ -92,55 +86,111 @@ export function About() {
       dropdown_menu.append(li);
     });
 
+    tr_btn_grp.append(tr_btn, toggle_dropdown, dropdown_menu);
+
     // test - preview of results 
-    console.log(dropdown_menu);
+    // console.log(dropdown_menu);
+    // console.log(tr_btn_grp);
 
-    modal.innerHTML = /*html*/ `
-    <div class="modal-dialog modal-dialog-centered modal-xl">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h1 class="modal-title fs-4" id="about-modal-title">About</h1>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-          </div>
-          <div class="modal-body">
-            <p>Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
-            <br><br> WERI's MAppFx platform offers an interactive map environment on a web page, enabling users to retrieve graphs of specific sites by clicking on map features such as points, polygons, or lines. MAppFx is a data visualization tool that incorporates a user-friendly interface, empowering users and stakeholders to make informed decisions regarding water resource management. The growing presence of online web applications with mapping and graphing features offers an opportunity to revolutionize hydrologic data analysis, enabling users to visualize complex datasets and derive valuable insights for decision-making. 
-            <br><br>The use of this dataset to develop these responsive features expands the interagency web map products and is now available on the Guam Hydrologic Survey (GHS) website (guamhydrologicsurvey.uog.edu), Guam’s online repository of pertinent hydrologic information established by Guam’s public laws. With the established website and new online interactive interface technology available, the GHS Information Management Team are now inclined to pursue the expansion of hydrologic web products and field survey database. The ever-growing GHS website product is a true testament to interagency collaboration, a great means of hydrologic information, dissemination, and scientific information source for aquifer management. 
-            </p>
-          </div>
-          <div class="modal-footer about-btns">
-            <!-- Dropdown for links to associated TRs -->
-            <div class="btn-group">
-              <a class="btn btn-primary" title="Coming soon!" href="https://guamhydrologicsurvey.uog.edu/" target="_blank" rel="noreferrer noopener" role="button">WERI Technical Report</a>
-              <button type="button" class="btn btn-primary dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false" title="View associated technical reports">
-                <span class="visually-hidden">Toggle Dropdown</span>
-              </button>
-              <ul class="dropdown-menu">
-                <li class="dropdown-item">Associated Technical Reports</li>
-                <li><hr class="dropdown-divider"></li>
-                <li><a class="dropdown-item" href="https://ghs-cdn.uog.edu/wp-content/databases/Library/PDFs/TRs/WERI%20TR%20180%20-%20Valerio%20et%20al%202023.pdf">WERI TR 180 (Valerio et al., 2023)</a></li>
-                <li><a class="dropdown-item" href="#" target="_blank" rel="noreferrer noopener" title="">WERI TR</a></li>
-                <li><a class="dropdown-item" href="#" target="_blank" rel="noreferrer noopener" title="">WERI TR</a></li>
-                <li><a class="dropdown-item" href="#" target="_blank" rel="noreferrer noopener" title="">WERI TR</a></li>
-              </ul>
-            </div>
+    // modal header 
+    const header = document.createElement("div");
+    header.className = "modal-header";
+    header.innerHTML = /*html*/ `
+    <h1 class="modal-title fs-4" id="about-modal-title">About</h1>
+    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+    `;
 
-            <!-- Dropdown for links to GHS maps libraries -->
-            <div class="dropdown">
-              <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                WERI Map Series
-              </button>
-              <ul class="dropdown-menu">
-                <li><a class="dropdown-item" target="_blank" rel="noreferrer noopener" href="https://guamhydrologicsurvey.uog.edu/mappfx-library/" title="MAppFx Library on GHS">MAppFx Library</a></li>
-                <li><a class="dropdown-item" target="_blank" rel="noreferrer noopener" href="https://guamhydrologicsurvey.uog.edu/web-mapps-library/" title="Web MApps Library on GHS">Web MApps</a></li>
-              </ul>
-            </div>
+    // modal body 
+    const body = document.createElement("div");
+    body.className = "modal-body";
+    body.innerHTML = /*html*/ `
+    <p>Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
+    <br><br> WERI's MAppFx platform offers an interactive map environment on a web page, enabling users to retrieve graphs of specific sites by clicking on map features such as points, polygons, or lines. MAppFx is a data visualization tool that incorporates a user-friendly interface, empowering users and stakeholders to make informed decisions regarding water resource management. The growing presence of online web applications with mapping and graphing features offers an opportunity to revolutionize hydrologic data analysis, enabling users to visualize complex datasets and derive valuable insights for decision-making. 
+    <br><br>The use of this dataset to develop these responsive features expands the interagency web map products and is now available on the Guam Hydrologic Survey (GHS) website (guamhydrologicsurvey.uog.edu), Guam’s online repository of pertinent hydrologic information established by Guam’s public laws. With the established website and new online interactive interface technology available, the GHS Information Management Team are now inclined to pursue the expansion of hydrologic web products and field survey database. The ever-growing GHS website product is a true testament to interagency collaboration, a great means of hydrologic information, dissemination, and scientific information source for aquifer management. 
+    </p>
+    `;
 
-          </div>
-        </div>
-      </div>
+    // modal footer 
+    const footer = document.createElement("div");
+    footer.className = "modal-footer about-btns";
+
+    // ghs map libraries
+    const map_libs = /*html*/ `
+    <div class="dropdown">
+      <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+        WERI Map Series
+      </button>
+      <ul class="dropdown-menu">
+        <li><a class="dropdown-item" target="_blank" rel="noreferrer noopener" href="https://guamhydrologicsurvey.uog.edu/mappfx-library/" title="MAppFx Library on GHS">MAppFx Library</a></li>
+        <li><a class="dropdown-item" target="_blank" rel="noreferrer noopener" href="https://guamhydrologicsurvey.uog.edu/web-mapps-library/" title="Web MApps Library on GHS">Web MApps</a></li>
+      </ul>
     </div>
     `;
+    
+    // first, add dropdown menu for assoc. technical reports 
+    footer.append(tr_btn_grp);
+
+    // lastly, add map libraries 
+    footer.insertAdjacentHTML("beforeend", map_libs);
+
+    // modal content - wrapper for everything (incl. header, body, footer)
+    const content = document.createElement("div");
+    content.className = "modal-content";
+    content.append(header, body, footer); // add modal elements to content
+    // console.log(content);
+
+    const content_wrapper = document.createElement("div");
+    content_wrapper.className = "modal-dialog modal-dialog-centered modal-xl";
+    content_wrapper.append(content);
+
+    modal.append(content_wrapper);
+    // console.log(modal);
+
+    // modal.innerHTML = /*html*/ `
+    // <div class="modal-dialog modal-dialog-centered modal-xl">
+    //     <div class="modal-content">
+    //       <div class="modal-header">
+    //         <h1 class="modal-title fs-4" id="about-modal-title">About</h1>
+    //         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+    //       </div>
+    //       <div class="modal-body">
+    //         <p>Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
+    //         <br><br> WERI's MAppFx platform offers an interactive map environment on a web page, enabling users to retrieve graphs of specific sites by clicking on map features such as points, polygons, or lines. MAppFx is a data visualization tool that incorporates a user-friendly interface, empowering users and stakeholders to make informed decisions regarding water resource management. The growing presence of online web applications with mapping and graphing features offers an opportunity to revolutionize hydrologic data analysis, enabling users to visualize complex datasets and derive valuable insights for decision-making. 
+    //         <br><br>The use of this dataset to develop these responsive features expands the interagency web map products and is now available on the Guam Hydrologic Survey (GHS) website (guamhydrologicsurvey.uog.edu), Guam’s online repository of pertinent hydrologic information established by Guam’s public laws. With the established website and new online interactive interface technology available, the GHS Information Management Team are now inclined to pursue the expansion of hydrologic web products and field survey database. The ever-growing GHS website product is a true testament to interagency collaboration, a great means of hydrologic information, dissemination, and scientific information source for aquifer management. 
+    //         </p>
+    //       </div>
+    //       <div class="modal-footer about-btns">
+    //         <!-- Dropdown for links to associated TRs -->
+    //         <div class="btn-group">
+    //           <a class="btn btn-primary" title="Coming soon!" href="https://guamhydrologicsurvey.uog.edu/" target="_blank" rel="noreferrer noopener" role="button">WERI Technical Report</a>
+    //           <button type="button" class="btn btn-primary dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false" title="View associated technical reports">
+    //             <span class="visually-hidden">Toggle Dropdown</span>
+    //           </button>
+    //           <ul class="dropdown-menu">
+    //             <li class="dropdown-item">Associated Technical Reports</li>
+    //             <li><hr class="dropdown-divider"></li>
+    //             <li><a class="dropdown-item" href="https://ghs-cdn.uog.edu/wp-content/databases/Library/PDFs/TRs/WERI%20TR%20180%20-%20Valerio%20et%20al%202023.pdf">WERI TR 180 (Valerio et al., 2023)</a></li>
+    //             <li><a class="dropdown-item" href="#" target="_blank" rel="noreferrer noopener" title="">WERI TR</a></li>
+    //             <li><a class="dropdown-item" href="#" target="_blank" rel="noreferrer noopener" title="">WERI TR</a></li>
+    //             <li><a class="dropdown-item" href="#" target="_blank" rel="noreferrer noopener" title="">WERI TR</a></li>
+    //           </ul>
+    //         </div>
+
+    //         <!-- Dropdown for links to GHS maps libraries -->
+    //         <div class="dropdown">
+    //           <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+    //             WERI Map Series
+    //           </button>
+    //           <ul class="dropdown-menu">
+    //             <li><a class="dropdown-item" target="_blank" rel="noreferrer noopener" href="https://guamhydrologicsurvey.uog.edu/mappfx-library/" title="MAppFx Library on GHS">MAppFx Library</a></li>
+    //             <li><a class="dropdown-item" target="_blank" rel="noreferrer noopener" href="https://guamhydrologicsurvey.uog.edu/web-mapps-library/" title="Web MApps Library on GHS">Web MApps</a></li>
+    //           </ul>
+    //         </div>
+
+    //     </div>
+    //   </div>
+    // </div>
+    // `;
 
     return modal;
 }
