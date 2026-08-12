133ae76a0fe4f3bcb0cada6ab0f6087351c859e8
Dannika <71364766+dannikate@users.noreply.github.com>
2026-08-12 16:35:47 +1000

    change var names
    remove modal_ prefix

--- client/src/components/Gallery2.js
+++ client/src/components/Gallery2.js
@@ -6,30 +6,30 @@ Description: Creates the Gallery component for site photos
 export function Gallery() {
     console.log("Added Gallery component");
 
-    const modal_outer = document.createElement("div");
+    const modal = document.createElement("div");
 
-    modal_outer.className = "modal fade";
-    modal_outer.tabIndex = -1;
-    modal_outer.id = "results";
+    modal.className = "modal fade";
+    modal.tabIndex = -1;
+    modal.id = "results";
 
-    modal_outer.setAttribute("aria-hidden", "true");
-    modal_outer.setAttribute("data-bs-backdrop", "true");
+    modal.setAttribute("aria-hidden", "true");
+    modal.setAttribute("data-bs-backdrop", "true");
 
-    const modal_inner = document.createElement("div");
-    modal_inner.className = "modal-dialog modal-dialog-centered modal-xl modal-dialog-scrollable";
+    const content_wrapper = document.createElement("div");
+    content_wrapper.className = "modal-dialog modal-dialog-centered modal-xl modal-dialog-scrollable";
 
-    const modal_content = document.createElement("div");
-    modal_content.className = "modal-content";
+    const content = document.createElement("div");
+    content.className = "modal-content";
 
-    const modal_header = document.createElement("div");
-    modal_header.className = "modal-header";
-    modal_header.innerHTML = /*html*/ `
+    const header = document.createElement("div");
+    header.className = "modal-header";
+    header.innerHTML = /*html*/ `
     <h5 class="modal-title" id="point-clicked"></h5>
     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
     `;
 
     // modal body contents 
-    const modal_body = document.createElement("div");
+    const body = document.createElement("div");
 
     const text_description = document.createElement("p");
     text_description.id = "text-description";
@@ -41,16 +41,16 @@ export function Gallery() {
     gallery_container.id = "gallery";
 
     // add text description, num photos, and gallery container to modal body 
-    modal_body.append(text_description, num_photos, gallery_container);
+    body.append(text_description, num_photos, gallery_container);
 
-    const modal_footer = document.createElement("div");
-    modal_footer.innerHTML = /*html*/ `
+    const footer = document.createElement("div");
+    footer.innerHTML = /*html*/ `
     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" title="Back to map view">Close</button>
     `;
 
-    modal_content.append(modal_header, modal_body, modal_footer);
-    modal_inner.append(modal_content);
-    modal_outer.append(modal_inner);
+    content.append(header, body, footer);
+    content_wrapper.append(content);
+    modal.append(content_wrapper);
 
-    return modal_outer;
+    return modal;
 }
