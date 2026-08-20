e0e921f47045fa357ab20585cc7cf454e431a6d9
Dannika <71364766+dannikate@users.noreply.github.com>
2026-08-20 13:57:57 +1000

    fix class name for dock, replace lucide icons for bs icons

--- client/src/components/Dock2.js
+++ client/src/components/Dock2.js
@@ -10,48 +10,49 @@ export function Dock() {
     dock_wrapper.className = "dock-wrapper";
 
     const dock = document.createElement("div");
-    dock.class = "dock";
+    dock.className = "dock";
     dock.id = "dock-control";
 
     // buttons 
     const toggle_layer = document.createElement("button");
     toggle_layer.id = "toggle-layer-btn";
     toggle_layer.innerHTML = /*html*/ `
-    <i data-lucide="layers"></i>
+    <i class="bi bi-stack"></i>
     <span class="dock-icon-tooltip">Toggle Layers</span>
     `;
 
     const zoom_in = document.createElement("button");
     zoom_in.id = "zoom-in-btn";
     zoom_in.innerHTML = /*html*/ `
-    <i data-lucide="zoom-in"></i>
+    <i class="bi bi-zoom-in"></i>
     <span class="dock-icon-tooltip">Zoom In</span>
     `;
 
     const zoom_out = document.createElement("button");
     zoom_out.id = "zoom-out-btn";
     zoom_in.innerHTML = /*html*/ `
-    <i data-lucide="zoom-out"></i>
+    <i class="bi bi-zoom-out"></i>
     <span class="dock-icon-tooltip">Zoom Out</span>
     `;
 
     const recenter = document.createElement("button");
     recenter.id = "recenter-btn";
     recenter.innerHTML = /*html*/ `
-    <i data-lucide="rotate-ccw"></i>
+    <i class="bi bi-arrow-clockwise"></i>
     <span class="dock-icon-tooltip">Recenter Map</span>
     `;
 
     const locate = document.createElement("button");
     locate.id = "locate-btn";
     locate.innerHTML = /*html*/ `
-    <i data-lucide="crosshair"></i>
+    <i class="bi bi-crosshair"></i>
     <span class="dock-icon-tooltip">Find My Location</span>
     `;
 
     dock.append(toggle_layer, zoom_in, zoom_out, recenter, locate);
+    dock_wrapper.append(dock);
 
-    console.log(dock);
+    console.log(dock_wrapper);
 
-    return dock;
+    return dock_wrapper;
 }
