95bbbd8aadfa99ae13dd043ac906b3316bc3f939
Dannika <71364766+dannikate@users.noreply.github.com>
2026-08-20 13:07:58 +1000

    add buttons for dock
    - set id's for each button
    - add inner HTML content (includes an icon from lucide and a tooltip)

--- client/src/components/Dock2.js
+++ client/src/components/Dock2.js
@@ -14,12 +14,44 @@ export function Dock() {
     dock.id = "dock-control";
 
     // buttons 
-    const toggle_legend = document.createElement("button");
     const toggle_layer = document.createElement("button");
+    toggle_layer.id = "toggle-layer-btn";
+    toggle_layer.innerHTML = /*html*/ `
+    <i data-lucide="layers"></i>
+    <span class="dock-icon-tooltip">Toggle Layers</span>
+    `;
+
     const zoom_in = document.createElement("button");
+    zoom_in.id = "zoom-in-btn";
+    zoom_in.innerHTML = /*html*/ `
+    <i data-lucide="zoom-in"></i>
+    <span class="dock-icon-tooltip">Zoom In</span>
+    `;
+
     const zoom_out = document.createElement("button");
+    zoom_out.id = "zoom-out-btn";
+    zoom_in.innerHTML = /*html*/ `
+    <i data-lucide="zoom-out"></i>
+    <span class="dock-icon-tooltip">Zoom Out</span>
+    `;
+
     const recenter = document.createElement("button");
+    recenter.id = "recenter-btn";
+    recenter.innerHTML = /*html*/ `
+    <i data-lucide="rotate-ccw"></i>
+    <span class="dock-icon-tooltip">Recenter Map</span>
+    `;
+
     const locate = document.createElement("button");
+    locate.id = "locate-btn";
+    locate.innerHTML = /*html*/ `
+    <i data-lucide="crosshair"></i>
+    <span class="dock-icon-tooltip">Find My Location</span>
+    `;
+
+    dock.append(toggle_layer, zoom_in, zoom_out, recenter, locate);
+
+    console.log(dock);
 
     return dock;
 }
