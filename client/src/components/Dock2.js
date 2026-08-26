fd084dd6a914271647d2341f53370b8a8a9e6a52
Dannika <71364766+dannikate@users.noreply.github.com>
2026-08-26 14:54:38 +1000

    add btn for transparency slider
    work in progress: plan to use bootstrap's range component

--- client/src/components/Dock2.js
+++ client/src/components/Dock2.js
@@ -51,7 +51,14 @@ export function Dock() {
     <span class="dock-icon-tooltip">Find My Location</span>
     `;
 
-    dock.append(toggle_layer, zoom_in, zoom_out, recenter, locate);
+    const transparency_slider = document.createElement("button");
+    transparency_slider.id = "transparency-btn";
+    transparency_slider.innerHTML = /*html*/ `
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-settings2-icon lucide-settings-2"><path d="M14 17H5"/><path d="M19 7h-9"/><circle cx="17" cy="17" r="3"/><circle cx="7" cy="7" r="3"/></svg>
+    <span class="dock-icon-tooltip">Adjust Polygon Transparency</span>
+    `;
+
+    dock.append(toggle_layer, zoom_in, zoom_out, recenter, locate, transparency_slider);
     dock_wrapper.append(dock);
 
     console.log(dock_wrapper);
