3d69ded2b6841a785577852e916995d6e73af748
Dannika <71364766+dannikate@users.noreply.github.com>
2026-09-09 00:32:07 +1000

    update video constant
    add different urls (regular url, embed url, and privacy url)

--- client/src/constants/videos.js
+++ client/src/constants/videos.js
@@ -7,86 +7,137 @@ export const videos = [
   {
     id: "G02",
     title: "Alutom Formation, Channel 10 | G02",
-    url: "https://www.youtube.com/watch?v=GcEAEHsr9LQ"
+    url: "https://www.youtube.com/watch?v=GcEAEHsr9LQ",
+    video_id: "GcEAEHsr9LQ",
+    embed_url: "https://www.youtube.com/embed/GcEAEHsr9LQ",
+    privacy_url: "https://www.youtube-nocookie.com/embed/GcEAEHsr9LQ"
   },
   {
     id: "G02",
     title: "Alutom Formation | G02",
-    url: "https://www.youtube.com/watch?v=PJm5wsJ8gAc"
+    url: "https://www.youtube.com/watch?v=PJm5wsJ8gAc",
+    video_id: "PJm5wsJ8gAc",
+    embed_url: "https://www.youtube.com/embed/PJm5wsJ8gAc",
+    privacy_url: "https://www.youtube-nocookie.com/embed/PJm5wsJ8gAc"
   },
   {
     id: "G26",
     title: "Piti Channel | G26",
-    url: "https://www.youtube.com/watch?v=F3rd5TVq2rE"
+    url: "https://www.youtube.com/watch?v=F3rd5TVq2rE",
+    video_id: "F3rd5TVq2rE",
+    embed_url: "https://www.youtube.com/embed/F3rd5TVq2rE",
+    privacy_url: "https://www.youtube-nocookie.com/embed/F3rd5TVq2rE"
   },
   {
     id: "P0046",
     title: "Badlands | P0046",
-    url: "https://www.youtube.com/watch?v=f4a9Oyjcxmw"
+    url: "https://www.youtube.com/watch?v=f4a9Oyjcxmw",
+    video_id: "f4a9Oyjcxmw",
+    embed_url: "https://www.youtube.com/embed/f4a9Oyjcxmw",
+    privacy_url: "https://www.youtube-nocookie.com/embed/f4a9Oyjcxmw"
   },
   {
     id: "P0053",
     title: "Southern Mountains Overlook from Para I Lalahi Ta Park | P0053",
-    url: "https://www.youtube.com/watch?v=sG6QBRQfoKY"
+    url: "https://www.youtube.com/watch?v=sG6QBRQfoKY",
+    video_id: "sG6QBRQfoKY",
+    embed_url: "https://www.youtube.com/embed/sG6QBRQfoKY",
+    privacy_url: "https://www.youtube-nocookie.com/embed/sG6QBRQfoKY"
   },
   {
     id: "P0053",
     title: "Southern Mountains Overlook (Northbound Drive) | P0053",
-    url: "https://www.youtube.com/watch?v=i8dHjM04xF4"
+    url: "https://www.youtube.com/watch?v=i8dHjM04xF4",
+    video_id: "i8dHjM04xF4",
+    embed_url: "https://www.youtube.com/embed/i8dHjM04xF4",
+    privacy_url: "https://www.youtube-nocookie.com/embed/i8dHjM04xF4"
   },
   {
     id: "P0053",
     title: "Southern Mountains Overlook Park | P0053",
-    url: "https://www.youtube.com/watch?v=zalDRl_Hm7o"
+    url: "https://www.youtube.com/watch?v=zalDRl_Hm7o",
+    video_id: "zalDRl_Hm7o",
+    embed_url: "https://www.youtube.com/embed/zalDRl_Hm7o",
+    privacy_url: "https://www.youtube-nocookie.com/embed/zalDRl_Hm7o"
   },
   {
     id: "P0055",
     title: "Two Lovers Point 1 | P0055",
-    url: "https://www.youtube.com/watch?v=xyXWbICOi_k"
+    url: "https://www.youtube.com/watch?v=xyXWbICOi_k",
+    video_id: "xyXWbICOi_k",
+    embed_url: "https://www.youtube.com/embed/xyXWbICOi_k",
+    privacy_url: "https://www.youtube-nocookie.com/embed/xyXWbICOi_k"
   },
   {
     id: "P0055",
     title: "Two Lovers Point 2 | P0055",
-    url: "https://www.youtube.com/watch?v=MPKxmNopqoU"
+    url: "https://www.youtube.com/watch?v=MPKxmNopqoU",
+    video_id: "MPKxmNopqoU",
+    embed_url: "https://www.youtube.com/embed/MPKxmNopqoU",
+    privacy_url: "https://www.youtube-nocookie.com/embed/MPKxmNopqoU"
   },
   {
     id: "P0054",
     title: "Oka Point | P0054",
-    url: "https://www.youtube.com/watch?v=3rHQxr0v6Ho"
+    url: "https://www.youtube.com/watch?v=3rHQxr0v6Ho",
+    video_id: "3rHQxr0v6Ho",
+    embed_url: "https://www.youtube.com/embed/3rHQxr0v6Ho",
+    privacy_url: "https://www.youtube-nocookie.com/embed/3rHQxr0v6Ho"
   },
   {
     id: "P0055",
     title: "Two Lovers Point 3 | P0055",
-    url: "https://www.youtube.com/watch?v=1Eu8K8TSyh0"
+    url: "https://www.youtube.com/watch?v=1Eu8K8TSyh0",
+    video_id: "1Eu8K8TSyh0",
+    embed_url: "https://www.youtube.com/embed/1Eu8K8TSyh0",
+    privacy_url: "https://www.youtube-nocookie.com/embed/1Eu8K8TSyh0"
   },
   {
     id: "P0061",
     title: "Priest Pools | P0061",
-    url: "https://www.youtube.com/watch?v=Hlg2OLoAacc"
+    url: "https://www.youtube.com/watch?v=Hlg2OLoAacc",
+    video_id: "Hlg2OLoAacc",
+    embed_url: "https://www.youtube.com/embed/Hlg2OLoAacc",
+    privacy_url: "https://www.youtube-nocookie.com/embed/Hlg2OLoAacc"
   },
   {
     id: "P0061",
     title: "Priest Pools (Drone POV) | P0061",
-    url: "https://www.youtube.com/watch?v=B1tiSwUytzE"
+    url: "https://www.youtube.com/watch?v=B1tiSwUytzE",
+    video_id: "B1tiSwUytzE",
+    embed_url: "https://www.youtube.com/embed/B1tiSwUytzE",
+    privacy_url: "https://www.youtube-nocookie.com/embed/B1tiSwUytzE"
   },
   {
     id: "P0066",
     title: "Bangi Island | P0066",
-    url: "https://www.youtube.com/watch?v=9aHKcUKyCuo"
+    url: "https://www.youtube.com/watch?v=9aHKcUKyCuo",
+    video_id: "9aHKcUKyCuo",
+    embed_url: "https://www.youtube.com/embed/9aHKcUKyCuo",
+    privacy_url: "https://www.youtube-nocookie.com/embed/9aHKcUKyCuo"
   },
   {
     id: "P0079",
     title: "Bottom of Thousand Steps | P0079",
-    url: "https://www.youtube.com/watch?v=Gy_d_BdiCpU"
+    url: "https://www.youtube.com/watch?v=Gy_d_BdiCpU",
+    video_id: "Gy_d_BdiCpU",
+    embed_url: "https://www.youtube.com/embed/Gy_d_BdiCpU",
+    privacy_url: "https://www.youtube-nocookie.com/embed/Gy_d_BdiCpU"
   },
   {
     id: "P0086",
     title: "View from Mt. Lam Lam | P0086",
-    url: "https://www.youtube.com/watch?v=PLl_tq1mfD8"
+    url: "https://www.youtube.com/watch?v=PLl_tq1mfD8",
+    video_id: "PLl_tq1mfD8",
+    embed_url: "https://www.youtube.com/embed/PLl_tq1mfD8",
+    privacy_url: "https://www.youtube-nocookie.com/embed/PLl_tq1mfD8"
   },
   {
     id: "P0091",
     title: "Rocks at Mt Lam Lam | P0091",
-    url: "https://www.youtube.com/watch?v=cX9gEThWMms"
+    url: "https://www.youtube.com/watch?v=cX9gEThWMms",
+    video_id: "cX9gEThWMms",
+    embed_url: "https://www.youtube.com/embed/cX9gEThWMms",
+    privacy_url: "https://www.youtube-nocookie.com/embed/cX9gEThWMms"
   }
 ];
