33ad5da8962290e4ca3dbdadd58727cc9a86a698
Dannika <71364766+dannikate@users.noreply.github.com>
2026-09-08 13:44:29 +1000

    change P0041 to correct feature, P0046

--- client/src/constants/videos.js
+++ client/src/constants/videos.js
@@ -5,88 +5,88 @@ Description:
 
 export const videos = [
   {
-    id: "G02",
-    title: "Alutom Formation, Channel 10 | G02",
-    url: "https://www.youtube.com/watch?v=GcEAEHsr9LQ"
+    "id": "G02",
+    "title": "Alutom Formation, Channel 10 | G02",
+    "url": "https://www.youtube.com/watch?v=GcEAEHsr9LQ"
   },
   {
-    id: "G02",
-    title: "Alutom Formation | G02",
-    url: "https://www.youtube.com/watch?v=PJm5wsJ8gAc"
+    "id": "G02",
+    "title": "Alutom Formation | G02",
+    "url": "https://www.youtube.com/watch?v=PJm5wsJ8gAc"
   },
   {
-    id: "G26",
-    title: "Piti Channel | G26",
-    url: "https://www.youtube.com/watch?v=F3rd5TVq2rE"
+    "id": "G26",
+    "title": "Piti Channel | G26",
+    "url": "https://www.youtube.com/watch?v=F3rd5TVq2rE"
   },
   {
-    id: "P0053",
-    title: "Southern Mountains Overlook from Para I Lalahi Ta Park | P0053",
-    url: "https://www.youtube.com/watch?v=sG6QBRQfoKY"
+    "id": "P0046",
+    "title": "Badlands | P0046",
+    "url": "https://www.youtube.com/watch?v=f4a9Oyjcxmw"
   },
   {
-    id: "P0041",
-    title: "Badlands | P0041",
-    url: "https://www.youtube.com/watch?v=f4a9Oyjcxmw"
+    "id": "P0053",
+    "title": "Southern Mountains Overlook from Para I Lalahi Ta Park | P0053",
+    "url": "https://www.youtube.com/watch?v=sG6QBRQfoKY"
   },
   {
-    id: "P0053",
-    title: "Southern Mountains Overlook (Northbound Drive) | P0053",
-    url: "https://www.youtube.com/watch?v=i8dHjM04xF4"
+    "id": "P0053",
+    "title": "Southern Mountains Overlook (Northbound Drive) | P0053",
+    "url": "https://www.youtube.com/watch?v=i8dHjM04xF4"
   },
   {
-    id: "P0053",
-    title: "Southern Mountains Overlook Park | P0053",
-    url: "https://www.youtube.com/watch?v=zalDRl_Hm7o"
+    "id": "P0053",
+    "title": "Southern Mountains Overlook Park | P0053",
+    "url": "https://www.youtube.com/watch?v=zalDRl_Hm7o"
   },
   {
-    id: "P0055",
-    title: "Two Lovers Point 1 | P0055",
-    url: "https://www.youtube.com/watch?v=xyXWbICOi_k"
+    "id": "P0055",
+    "title": "Two Lovers Point 1 | P0055",
+    "url": "https://www.youtube.com/watch?v=xyXWbICOi_k"
   },
   {
-    id: "P0055",
-    title: "Two Lovers Point 2 | P0055",
-    url: "https://www.youtube.com/watch?v=MPKxmNopqoU"
+    "id": "P0055",
+    "title": "Two Lovers Point 2 | P0055",
+    "url": "https://www.youtube.com/watch?v=MPKxmNopqoU"
   },
   {
-    id: "P0054",
-    title: "Oka Point | P0054",
-    url: "https://www.youtube.com/watch?v=3rHQxr0v6Ho"
+    "id": "P0054",
+    "title": "Oka Point | P0054",
+    "url": "https://www.youtube.com/watch?v=3rHQxr0v6Ho"
   },
   {
-    id: "P0055",
-    title: "Two Lovers Point 3 | P0055",
-    url: "https://www.youtube.com/watch?v=1Eu8K8TSyh0"
+    "id": "P0055",
+    "title": "Two Lovers Point 3 | P0055",
+    "url": "https://www.youtube.com/watch?v=1Eu8K8TSyh0"
   },
   {
-    id: "P0061",
-    title: "Priest Pools | P0061",
-    url: "https://www.youtube.com/watch?v=Hlg2OLoAacc"
+    "id": "P0061",
+    "title": "Priest Pools | P0061",
+    "url": "https://www.youtube.com/watch?v=Hlg2OLoAacc"
   },
   {
-    id: "P0061",
-    title: "Priest Pools (Drone POV) | P0061",
-    url: "https://www.youtube.com/watch?v=B1tiSwUytzE"
+    "id": "P0061",
+    "title": "Priest Pools (Drone POV) | P0061",
+    "url": "https://www.youtube.com/watch?v=B1tiSwUytzE"
   },
   {
-    id: "P0066",
-    title: "Bangi Island | P0066",
-    url: "https://www.youtube.com/watch?v=9aHKcUKyCuo"
+    "id": "P0066",
+    "title": "Bangi Island | P0066",
+    "url": "https://www.youtube.com/watch?v=9aHKcUKyCuo"
   },
   {
-    id: "P0079",
-    title: "Bottom of Thousand Steps | P0079",
-    url: "https://www.youtube.com/watch?v=Gy_d_BdiCpU"
+    "id": "P0079",
+    "title": "Bottom of Thousand Steps | P0079",
+    "url": "https://www.youtube.com/watch?v=Gy_d_BdiCpU"
   },
   {
-    id: "P0086",
-    title: "View from Mt. Lam Lam | P0086",
-    url: "https://www.youtube.com/watch?v=PLl_tq1mfD8"
+    "id": "P0086",
+    "title": "View from Mt. Lam Lam | P0086",
+    "url": "https://www.youtube.com/watch?v=PLl_tq1mfD8"
   },
   {
-    id: "P0091",
-    title: "Rocks at Mt Lam Lam | P0091",
-    url: "https://www.youtube.com/watch?v=cX9gEThWMms"
+    "id": "P0091",
+    "title": "Rocks at Mt Lam Lam | P0091",
+    "url": "https://www.youtube.com/watch?v=cX9gEThWMms"
   }
 ];
