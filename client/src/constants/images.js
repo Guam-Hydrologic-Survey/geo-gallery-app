3af3f838a64c83d2343f1e4992259a5367ac0c1c
Dannika <71364766+dannikate@users.noreply.github.com>
2026-09-09 00:35:24 +1000

    create image list for cross sections
    work in progress

---
+++ client/src/constants/images.js
@@ -0,0 +1,32 @@
+/* 
+images.js
+Description: 
+*/
+
+export const cross_section_images = [ 
+    {
+        cross_section: "A",
+        description: "",
+        image: "geo-map-siegrist-2007-cross-section-a.png"
+    },
+    {
+        cross_section: "B",
+        description: "",
+        image: "geo-map-siegrist-2007-cross-section-b.png"
+    },
+    {
+        cross_section: "C",
+        description: "",
+        image: "geo-map-siegrist-2007-cross-section-c.png"
+    },
+    {
+        cross_section: "D",
+        description: "",
+        image: "geo-map-siegrist-2007-cross-section-d.png"
+    },
+    {
+        cross_section: "E",
+        description: "",
+        image: "geo-map-siegrist-2007-cross-section-e.png"
+    }
+]
