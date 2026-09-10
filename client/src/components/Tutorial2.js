f20b6dfa9f22215900ac68824ca38fc07060bddd
Dannika <71364766+dannikate@users.noreply.github.com>
2026-09-10 15:45:02 +1000

    add to tutorial modal body
    instructions on available controls on dock

--- client/src/components/Tutorial2.js
+++ client/src/components/Tutorial2.js
@@ -27,6 +27,16 @@ export function Tutorial() {
     body.innerHTML = /*html*/ `
     <p>Welcome to the Geology Photo Gallery. Brought to you by the Guam Hydrologic Survey Information Management Team at WERI.</p>
     <p>To use this map, click on a <span class="term" data-bs-toggle="popover" data-bs-trigger="hover focus" data-bs-title="Polygons" data-bs-content="Colored shapes covering areas on the map">polygon</span> or <span class="term" data-bs-toggle="popover" data-bs-trigger="hover focus" data-bs-title="Points" data-bs-content="Markers with different icons overlaying the map">point</span> to view available photos from that site.</p>
+    <hr>
+    <p>To adjust the visibility of the map features, check out the <span class="term" data-bs-toggle="popover" data-bs-trigger="hover focus" data-bs-title="Layer Toggle" data-bs-content="A side panel containing options to change the base map tiles and toggle feature layers on and off">layer toggle</span> on the dock at the bottom.</p>
+    <hr>
+    <p>To adjust the transparency of the polygon layers, check out the <span class="term" data-bs-toggle="popover" data-bs-trigger="hover focus" data-bs-title="Adjust Layer Transparency" data-bs-content="A side panel containing a slider to change the opacity of the polygon layers. It ranges from 0% to 100% and provides the option to reset to the default polygon layer transparency.">layer transparency slider</span> on the dock at the bottom.</p>
+    <hr>
+    <p>To view cross sections of the aquifer, check out the <span class="term" data-bs-toggle="popover" data-bs-trigger="hover focus" data-bs-title="View Cross Sections of the Aquifer" data-bs-content="Adds the cross sections of the aquifer as lines. To view a specific cross section, click on one of the blue lines on the map">cross sections viewer</span> on the dock at the bottom.</p>
+    <hr> 
+    <p>Control the zoom levels of the map using the magnifying glass buttons on the dock at the bottom</p>
+    <hr>
+    <p>To recenter the map back to its default position, click on the recenter button on the dock at the bottom</p>
     `;
 
     // steps
