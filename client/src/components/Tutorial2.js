448ac6871a7e5171b4ee8081eb7fcf1aac60cfc6
Dannika <71364766+dannikate@users.noreply.github.com>
2026-09-10 23:49:03 +1000

    adjust tutorial and styles

--- client/src/components/Tutorial2.js
+++ client/src/components/Tutorial2.js
@@ -25,32 +25,34 @@ export function Tutorial() {
     const body = document.createElement("div");
     body.className = "modal-body";
     body.innerHTML = /*html*/ `
-    <p>Welcome to the Geology Photo Gallery. Brought to you by the Guam Hydrologic Survey Information Management Team at WERI.</p>
-    <p>To use this map, click on a <span class="term" data-bs-toggle="popover" data-bs-trigger="hover focus" data-bs-title="Polygons" data-bs-content="Colored shapes covering areas on the map">polygon</span> or <span class="term" data-bs-toggle="popover" data-bs-trigger="hover focus" data-bs-title="Points" data-bs-content="Markers with different icons overlaying the map">point</span> to view available photos from that site.</p>
-    <hr>
-    <p>To adjust the visibility of the map features, check out the <span class="term" data-bs-toggle="popover" data-bs-trigger="hover focus" data-bs-title="Layer Toggle" data-bs-content="A side panel containing options to change the base map tiles and toggle feature layers on and off">layer toggle</span> on the dock at the bottom.</p>
-    <hr>
-    <p>To adjust the transparency of the polygon layers, check out the <span class="term" data-bs-toggle="popover" data-bs-trigger="hover focus" data-bs-title="Adjust Layer Transparency" data-bs-content="A side panel containing a slider to change the opacity of the polygon layers. It ranges from 0% to 100% and provides the option to reset to the default polygon layer transparency.">layer transparency slider</span> on the dock at the bottom.</p>
-    <hr>
-    <p>To view cross sections of the aquifer, check out the <span class="term" data-bs-toggle="popover" data-bs-trigger="hover focus" data-bs-title="View Cross Sections of the Aquifer" data-bs-content="Adds the cross sections of the aquifer as lines. To view a specific cross section, click on one of the blue lines on the map">cross sections viewer</span> on the dock at the bottom.</p>
-    <hr> 
-    <p>Control the zoom levels of the map using the magnifying glass buttons on the dock at the bottom</p>
-    <hr>
-    <p>To recenter the map back to its default position, click on the recenter button on the dock at the bottom</p>
+    <p>Welcome to the Geology Photo Gallery!</p>
+    <p class="text-italicize">Here's a quick guide on the app's features:</p>
+    <ul>
+        <li>To use this map, click on a <span class="term" data-bs-toggle="popover" data-bs-trigger="hover focus" data-bs-title="Polygons" data-bs-content="Colored shapes covering areas on the map">polygon</span> or <span class="term" data-bs-toggle="popover" data-bs-trigger="hover focus" data-bs-title="Points" data-bs-content="Markers with different icons overlaying the map">point</span> to view available photos from that site.</li>
+        <li>To adjust the visibility of the map features, check out the <span class="term" data-bs-toggle="popover" data-bs-trigger="hover focus" data-bs-title="Layer Toggle" data-bs-content="A side panel containing options to change the base map tiles and toggle feature layers on and off">layer toggle</span> on the dock at the bottom.</li>
+        <li>To adjust the transparency of the polygon layers, check out the <span class="term" data-bs-toggle="popover" data-bs-trigger="hover focus" data-bs-title="Adjust Layer Transparency" data-bs-content="A side panel containing a slider to change the opacity of the polygon layers. It ranges from 0% to 100% and provides the option to reset to the default polygon layer transparency.">layer transparency slider</span> on the dock at the bottom.</li>
+        <li>To view cross sections of the aquifer, check out the <span class="term" data-bs-toggle="popover" data-bs-trigger="hover focus" data-bs-title="View Cross Sections of the Aquifer" data-bs-content="Adds the cross sections of the aquifer as lines. To view a specific cross section, click on one of the blue lines on the map">cross sections viewer</span> on the dock at the bottom.</li>
+        <li>Control the zoom levels of the map using the magnifying glass buttons on the dock at the bottom</li>
+        <li>To recenter the map back to its default position, click on the recenter button on the dock at the bottom</li>
+    </ul>
     `;
 
     // steps
     const steps = [
         {
-            title: "1",
-            body: "Lorem ipsom dolor sit amet"
+            title: "Welcome to the Geo Gallery!",
+            body: "This is a quick tour of the app's features. It only takes a few seconds."
         },
         {
-            title: "2",
-            body: "Lorem ipsom dolor sit amet"
+            title: "The navigation bar",
+            body: "It sits at the top right of the page. It houses information about the project, and links to maps and related resources on the Guam Hydrologic Survey website. Here, you can also find the Legend, which provides the key to the geologic features on the map."
         },
         {
-            title: "3",
+            title: "The dock",
+            body: "It sits at the bottom center of the page. It contains all the map controls, such as the layer visibility toggle, the layer transparency adjustment, cross sections of the aquifer, zoom, recenter, and a geo locator."
+        },
+        {
+            title: "",
             body: "Lorem ipsom dolor sit amet"
         }
     ];
